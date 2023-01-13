47a695128752550fd54f6dfbf7b08956b430e3bd
LupoArticoblu <wolf94-@libero.it>
2023-01-13 17:10:34 +0100

    [fix] web.php

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', FumettoController::class, 'index')->name('home');
+Route::get('/', [FumettoController::class, 'index'])->name('home');
 
 Route::resource('fumetti', FumettoController::class);
